bc994e90025eb3dee0ae1554d36859cbff61fede
twinogre <glad2you@gmail.com>
2024-02-16 17:05:30 +0900

    products_service_delete

--- src/main/java/com/eCommers/ebook/domain/products/service/ProductsService.java
+++ src/main/java/com/eCommers/ebook/domain/products/service/ProductsService.java
@@ -43,4 +43,8 @@ public class ProductsService {
 
         this.productsRepository.save(productsModify);
     }
+
+    public void deleteProducts(Products products){
+        this.productsRepository.delete(products);
+    }
 }
